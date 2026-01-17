5118e9362c8d32a2edaf4dbd913d0ccf0aba7f55
adhav <adhavanbalagan@gmail.com>
2026-01-17 23:43:08 +0800

    L18 Submission

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState, useEffect} from 'react';
+import { FlatList, StatusBar, Text, Image, TextInput, View} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// Create a new variable named originalData
+let originalData = [];
+
+const App = () => {
+    const [myData, setMyData] = useState([]);
+
+    // Exercise 1B - add useEffect()
+    useEffect(() => {
+        // Add fetch() - Exercise 1A
+        const myurl = "https://onlinettcalbumappwebservice1.onrender.com/allalbums"
+        fetch(myurl)
+            .then((response)=>{
+                return response.json();
+            })
+            .then((myJson)=>{
+                setMyData(myJson);
+                originalData = myJson;
+            })
+    }, []);
+
+    const FilterData = (text) => {
+        if(text!='') {
+            let myFilteredData = originalData.filter((item) =>
+                item.album_name.toLowerCase().includes(text.toLowerCase()));
+            setMyData(myFilteredData);
+        }
+        else {
+            setMyData(originalData);
+        }
+    }
+
+    const renderItem = ({item, index}) => {
+        // return (
+        //     <View>
+        //         <Text style={{borderWidth:1}}>{item.album_name}</Text>
+        //     </View>
+        // );
+        return (
+            <View style={{
+                flexDirection: "row",
+                borderWidth: 1,
+                padding: 10,
+                margin: 5,
+                alignItems: "center"
+            }}>
+
+                <Image
+                    source={{ uri: item.album_cover_art }}
+                    style={{
+                        width: 80,
+                        height: 110,
+                        marginRight: 10
+                    }}
+                    resizeMode="contain"
+                />
+
+                <Text>
+                    {item.album_name}
+                </Text>
+
+            </View>
+        );
+    };
+
+    return (
+        <View>
+            <StatusBar/>
+            <Text>Search:</Text>
+            {/*<TextInput style={{borderWidth:1}}/>*/}
+            <TextInput style={{borderWidth:1}} onChangeText={(text)=> {FilterData(text)}}></TextInput>
+            <FlatList data={myData} renderItem={renderItem} />
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
